653583c239f22a5fcba21bbc59d4b1ba887510d4
Ahmed Essam <ahmedessam.052005@gmail.com>
2025-02-25 21:01:00 +0200

    Marble to Card (without subclasses)
    Read essam's History description of this commit.

--- src/model/card/Card.java
+++ src/model/card/Card.java
@@ -2,4 +2,48 @@ package model.card;
 
 public class Card {
 
+<<<<<<< Updated upstream
+=======
+public abstract class Card{
+	private final String name;
+	private final String description;
+	protected BoardManager boardManager;
+	protected GameManager gameManager;
+	
+	//SUBCLASSES ARE NOT DONE PLEASE READ THE CARD 
+	//CLASS TO MAKE THE SUBCLASSES STANDARD AND WILD.
+	
+	public Card(String name, String description, BoardManager boardManager, GameManager gameManager) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.boardManager = new Board();
+		this.gameManager = new Game();
+	}
+
+	public BoardManager getBoardManager() {
+		return boardManager;
+	}
+
+	public void setBoardManager(BoardManager boardManager) {
+		this.boardManager = boardManager;
+	}
+
+	public GameManager getGameManager() {
+		return gameManager;
+	}
+
+	public void setGameManager(GameManager gameManager) {
+		this.gameManager = gameManager;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+>>>>>>> Stashed changes
 }
