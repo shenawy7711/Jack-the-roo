ef3865119dc7317070eb15c514cc74b623c1e57f
shenawy7711 <shenawy141516@gmail.com>
2025-03-01 23:14:29 +0200

    4.19, 4.20 and 4.21 done but constructers not finished

--- src/engine/board/Board.java
+++ src/engine/board/Board.java
@@ -1,5 +1,36 @@
 package engine.board;
 
+import java.util.ArrayList;
+
+import engine.GameManager;
+import model.Colour;
+
 public class Board implements BoardManager {
+	private final GameManager gameManager;
+	private ArrayList<Cell> track;
+	private ArrayList<SafeZone> safeZones;
+	private int splitDistance;
 	
+	public Board(ArrayList<Colour> colourOrder, GameManager gameManager) {
+		this.gameManager=gameManager;
+		track=new ArrayList<>();
+		safeZones=new ArrayList<>();
+		splitDistance=3;
+		
+	}
+	public int getSplitDistance() {
+		return splitDistance;
+	}
+	public void setSplitDistance(int splitDistance) {
+		this.splitDistance = splitDistance;
+	}
+	public ArrayList<Cell> getTrack() {
+		return track;
+	}
+	public ArrayList<SafeZone> getSafeZones() {
+		return safeZones;
+	}
+	public void assignTrapCell() {
+		
+	}
 }
