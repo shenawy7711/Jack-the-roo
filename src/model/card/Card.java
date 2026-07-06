83b2cd326e91e41587780aa340ac1c0549a8dd42
shenawy7711 <shenawy141516@gmail.com>
2026-07-06 23:18:10 +0300

    cleaning code up

--- src/model/card/Card.java
+++ src/model/card/Card.java
@@ -29,26 +29,6 @@ public abstract class Card {
     public String getDescription() {
         return description;
     }
-<<<<<<< Updated upstream
-=======
-    
-    public abstract void act(ArrayList<Marble> marbles) throws ActionException, InvalidMarbleException;
-    
-    public boolean validateMarbleSize(ArrayList<Marble> marbles) {
-        return marbles.size() == 1;
-    }
-    
-    public boolean validateMarbleColours(ArrayList<Marble> marbles) {
-        Colour ownerColour = gameManager.getActivePlayerColour();
-        boolean sameColour = true;
-        for (Marble marble : marbles) {
-            if (marble.getColour() != ownerColour) {
-                sameColour = false;
-            }
-        }
-        return sameColour;
-    }
->>>>>>> Stashed changes
     
     public abstract void act(ArrayList<Marble> marbles) throws ActionException, InvalidMarbleException;
     
